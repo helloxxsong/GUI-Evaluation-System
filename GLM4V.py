916a4bcf9341ffb80b1e0020ef6fb6a25f59ddc4
宋一凡 <121983035+helloxxsong@users.noreply.github.com>
2024-03-05 19:19:14 +0800

    Create GLM4V.py

---
+++ GLM4V.py
@@ -0,0 +1,59 @@
+from zhipuai import ZhipuAI
+
+client = ZhipuAI(
+    api_key="126907188f35f86595a5f17d92785350.Urr49pRRttcbw9yO"
+)
+
+
+def get_vision_completion(user_prompt, image_url):
+    response = client.chat.completions.create(
+        model="glm-4v",  # 填写需要调用的模型名称
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": f"{user_prompt}"
+                    },
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": f"{image_url}"
+                        }
+                    }
+                ]
+            }
+        ]
+    )
+    return response.choices[0].message.content
+
+
+def get_visions_completion(user_prompt, image_url1, image_url2):
+    response = client.chat.completions.create(
+        model="glm-4v",  # 填写需要调用的模型名称
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": f"{user_prompt}"
+                    },
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": f"{image_url1}"
+                        }
+                    },
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": f"{image_url2}"
+                        }
+                    }
+                ]
+            }
+        ]
+    )
+    return response.choices[0].message.content
