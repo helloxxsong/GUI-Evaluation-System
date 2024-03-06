2777e6433eaa0c3c4304536be1917e7dbfc1234f
宋一凡 <121983035+helloxxsong@users.noreply.github.com>
2024-03-06 09:32:35 +0800

    Update GPT4V_local.py

--- GPT4V_local.py
+++ GPT4V_local.py
@@ -2,7 +2,7 @@ import base64
 import requests
 
 # OpenAI API Key
-api_key = "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"
+api_key = ""
 
 
 # Function to encode the image
@@ -11,10 +11,7 @@ def encode_image(image_path):
         return base64.b64encode(image_file.read()).decode('utf-8')
 
 
-def single_image(image_path):
-    # Getting the base64 string
-    base64_image = encode_image(image_path)
-
+def single_image(prompt, image_path):
     headers = {
         "Content-Type": "application/json",
         "Authorization": f"Bearer {api_key}"
@@ -28,18 +25,65 @@ def single_image(image_path):
                 "content": [
                     {
                         "type": "text",
-                        "text": "What’s in this image?"
+                        "text": f"{prompt}"
                     },
                     {
                         "type": "image_url",
                         "image_url": {
-                            "url": f"data:image/jpeg;base64,{base64_image}"
+                            "url": f"data:image/jpeg;base64,{encode_image(image_path)}"
                         }
                     }
                 ]
             }
         ],
-        "max_tokens": 300
+        "max_tokens": 1000
+    }
+
+    for count in range(5):
+        try:
+            response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload, timeout=40)
+            return response.json()
+        except requests.exceptions.Timeout:
+            print(f"请求超时，正在重试...（{count + 1} / {5}）")
+            continue
+        except requests.exceptions.RequestException as e:
+            print(f"请求遇到错误：{e}")
+            break
+    return None
+
+
+def double_images(prompt, image_path1, image_path2):
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {api_key}"
+    }
+
+    payload = {
+        "model": "gpt-4-vision-preview",
+        "messages": [
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": f"{prompt}"
+                    },
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": f"data:image/jpeg;base64,{encode_image(image_path1)}"
+                        }
+                    },
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": f"data:image/jpeg;base64,{encode_image(image_path2)}"
+                        }
+                    },
+                ]
+            }
+        ],
+        "max_tokens": 1000
     }
 
     for count in range(5):
